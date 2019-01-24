9db14993b88b51e29e530ba9bb2c25bcf71e0722
KseniaZakh <cassie3@yandex.ru>
2019-01-24 20:22:43 +0500

    header

--- js/common.js
+++ js/common.js
@@ -1,9 +1,23 @@
 function resize() {
-		if ($("div.container > footer").length!=0) {
-			$("div.container").css({"padding-bottom" : ($("div.container > footer").outerHeight())});
-		}
+
 }
 
 $(document).ready(function() {
-	
+	$("ul.language > li:first-child").on("click", function() {
+		$("ul.language > li:not(:first-child)").toggle();
+	})
+
+	$("ul.mob_menu").click(function() {
+		if ($(this).hasClass("opened")) {
+			$(this).removeClass("opened");
+		}
+		else {
+			$(this).addClass("opened");
+		}
+	})
+
+	if ($("div.container > footer").length!=0) {
+		$("div.container").css({"padding-bottom" : ($("div.container > footer").outerHeight())});
+	}
+
 })
