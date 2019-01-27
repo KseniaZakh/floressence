8d87a555c2ff237e5c981b73905543b85c753e3c
KseniaZakh <cassie3@yandex.ru>
2019-01-27 16:27:54 +0500

    nav animation

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 function resize() {
-
+	if ($("nav").length!=0 &&
+	$("ul.mob_menu").length!=0) {
+		if ($("nav").hasClass("mob") &&
+		 $("ul.mob_menu").hasClass("opened")) {
+					$("nav").removeClass("mob");
+					$("ul.mob_menu").removeClass("opened");
+			}
+		}
 }
 
 $(document).ready(function() {
@@ -8,13 +15,39 @@ $(document).ready(function() {
 	})
 
 	$("ul.mob_menu").click(function() {
+		if ($("nav").length!=0) {
 		if ($(this).hasClass("opened")) {
 			$(this).removeClass("opened");
-		}
+			$("nav").removeClass("mob");
+			$("div.container").css({"right" : "0"});
+			$("header").css({"right" : "0"});
+			}
 		else {
 			$(this).addClass("opened");
-		}
-	})
+			$("nav").addClass("mob");
+			if (window.matchMedia("(max-width: 480px)").matches) {
+				$("div.container").css({"right" : "200px"});
+				$("header").css({"right" : "200px"});
+				}
+			else {
+				$("div.container").css({"right" : "320px"});
+				$("header").css({"right" : "320px"});
+				}
+
+			}
+			}
+		});
+		$("nav").click(function() {
+			if ($(this).hasClass("mob")) {
+				$(this).removeClass("mob");
+				$("ul.mob_menu").removeClass("opened");
+				$("div.container").css({"right" : "0"});
+				$("header").css({"right" : "0"});
+				}
+
+
+		});
+		return false;
 
 	if ($("div.container > footer").length!=0) {
 		$("div.container").css({"padding-bottom" : ($("div.container > footer").outerHeight())});
