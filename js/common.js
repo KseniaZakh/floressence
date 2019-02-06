f78eba9b5b959df10c3c158de49e493f821fa1af
KseniaZakh <cassie3@yandex.ru>
2019-02-06 17:58:53 +0500

    new nav

--- js/common.js
+++ js/common.js
@@ -1,48 +1,32 @@
 function resize() {
-	if ($("nav").length!=0 &&
-	$("ul.mob_menu").length!=0) {
-		if ($("nav").hasClass("mob") &&
-		 $("ul.mob_menu").hasClass("opened")) {
-					$("nav").removeClass("mob");
-					$("ul.mob_menu").removeClass("opened");
+	if ($("nav").length!=0 && $("div.container").hasClass("opened")) {
+		// if ($("nav").hasClass("mob") &&
+		//  $("ul.mob_menu").hasClass("opened")) {
+		// 			$("nav").removeClass("mob");
+		// 			$("ul.mob_menu").removeClass("opened");
 					$("div.container").removeClass("opened");
-					$("header").removeClass("opened");
+					// $("header").removeClass("opened");
 			}
 		}
-}
 
 $(document).ready(function() {
-	$("ul.mob_menu").click(function() {
-		if ($("nav").length!=0) {
-			if ($(this).hasClass("opened")) {
-				$(this).removeClass("opened");
-				$("nav").removeClass("mob");
-				$("div.container").removeClass("opened");
-				$("header").removeClass("opened");
-				}
-			else {
-				$(this).addClass("opened");
-				$("nav").addClass("mob");
-				$("div.container").addClass("opened");
-				$("header").addClass("opened");
-				}
-			}
-		});
-		$("nav").click(function() {
-			if ($(this).hasClass("mob")) {
-				$(this).removeClass("mob");
-				$("ul.mob_menu").removeClass("opened");
-				$("div.container").removeClass("opened");
-				$("header").removeClass("opened");
-				}
-		});
+			$("header > div > ul").click(function() {
+				if ($("div.container > nav").length!=0) {
+					if ($("div.container").hasClass("opened")) {
+						$("div.container").removeClass("opened");
+						}
+					else {
+						$("div.container").addClass("opened");
+						}
+					}
+				});
 
-		$("div.container > div.content > div.video > span").on("click", function() {
-		setTimeout(function(){
-			$("div.container > div.content > div.video > video").get(0).play();
-		}, 300);
-		});
-	$("div.container > div.content > div.video > span").click();
+				$("div.container > div.content > div.video > span").on("click", function() {
+				setTimeout(function(){
+					$("div.container > div.content > div.video > video").get(0).play();
+				}, 300);
+				});
+			$("div.container > div.content > div.video > span").click();
 
-		return false;
-});
+			return false;
+			});
