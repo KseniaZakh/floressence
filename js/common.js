d3a7479ca31bdb1bfad53443408c5a9e84e1216f
KseniaZakh <cassie3@yandex.ru>
2019-03-26 17:08:08 +0500

    about company

--- js/common.js
+++ js/common.js
@@ -5,22 +5,41 @@ function resize() {
 		}
 
 $(document).ready(function() {
+
+				$('.number').each(function () {
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			}, {
+				duration: 4000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+			});
+
 			$("ul.mob_menu").click(function() {
 				if ($("div.container > nav").length!=0) {
 					$("div.container").toggleClass("opened");
 					}
 				});
 
-				$("div.container > div.content > div.video > span").on("click", function() {
+				$("div.container > article > div.video > span").on("click", function() {
 				setTimeout(function(){
-					$("div.container > div.content > div.video > video").get(0).play();
+					$("div.container > article > div.video > video").get(0).play();
 				}, 300);
 				});
-			$("div.container > div.content > div.video > span").click();
+			$("div.container > article > div.video > span").click();
 
 			if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		$("div.preview").css("background-attachment", "fixed");
 		}
+
+		$("div.photo_text > div > div").click(function() {
+			if ($("div.photo_text > div > img").length!=0) {
+				$("div.photo_text > div").addClass("opened");
+				}
+			});
 
 			return false;
 			});
